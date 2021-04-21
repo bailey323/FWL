3831c28c59588e20d89eda0c3a6fdca885707982
bailey323 <58584739+bailey323@users.noreply.github.com>
2021-04-21 16:05:20 +0100

    commit

---
+++ Public/js/index.js
@@ -0,0 +1,61 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "flex";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
+
+function forgotpassword() {
+
+  var auth = firebase.auth();
+  var userEmail = document.getElementById("email_field").value;
+
+  auth.sendPasswordResetEmail(userEmail).then(function() {
+
+    window.alert('Please check your emails for your password reset link. Do not forget to check your junk');
+    // Email sent.
+  }).catch(function(error) {
+
+    window.alert('Email does not exist in database');
+  // An error happened.
+});
+}
